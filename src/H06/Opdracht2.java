6cebcf376a8c92859fefaaf7b6178671a947178d
Smisidjan <Sarai.misidjan@hotmail.com>
2019-09-26 10:14:32 +0200

    h6 opdracht 2

--- src/H06/Opdracht2.java
+++ src/H06/Opdracht2.java
@@ -3,17 +3,20 @@ package H06;
 import java.applet.Applet;
 import java.awt.*;
 
-public class Opdracht2 extends Applet {
-    int getal1, getal2;
+public class Opdracht2 extends Applet{
+    int seconden, uur, dag, jaar;
 
     public void init() {
-        getal1 = 15;
-        getal2 = 30;
-
+        seconden = 60;
+        uur = 60 ;
+        dag = 24;
+        jaar = 365;
     }
 
     public void paint(Graphics g) {
-        g.drawString("De uitkomst is: " +(getal1 - getal2), 20, 20 );
+        g.drawString("Seconden in een uur: " + seconden * 60, 20, 20);
+        g.drawString("Seconden in een dag: " + seconden * uur * dag, 20, 35);
+        g.drawString( "Seconden in een jaar: " + jaar * 86400, 20, 50);
 
     }
 }
