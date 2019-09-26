37feb6d57ea9499d7bb6c267d8a8bed6b236ba7f
Smisidjan <Sarai.misidjan@hotmail.com>
2019-09-26 10:00:14 +0200

    h6 opdracht 3

---
+++ src/H06/Opdracht3.java
@@ -0,0 +1,19 @@
+package H06;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+    int getal1, getal2;
+
+    public void init() {
+        getal1 = 15;
+        getal2 = 30;
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("De uitkomst is: " +(getal1 - getal2), 20, 20 );
+
+    }
+}
