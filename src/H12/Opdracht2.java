423cbabf53941cee5e3ec7879c1e38389abe972f
Smisidjan <Sarai.misidjan@hotmail.com>
2019-11-11 09:01:59 +0100

    h12 opdracht 2

--- src/H12/Opdracht2.java
+++ src/H12/Opdracht2.java
@@ -6,20 +6,15 @@ import java.awt.*;
 public class Opdracht2 extends Applet {
     Button[] knop;
 
-
     public void init() {
         knop = new Button[26];
 
         for (int i = 1; i < knop.length; i++) {
-
             knop[i] = new Button(String.valueOf(i));
             System.out.println(knop[i]);
             add(knop[i]);
         }
-
     }
-
-    public void paint(Graphics g) {
-
-    }
+    
+    public void paint(Graphics g) {}
 }
